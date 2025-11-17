df33ed9c6a06fadfec63a40c56edaf4f01c0ecee
ruzen42- <malledeferlive@gmail.com>
2025-11-17 17:05:44 +0500

    feat(first version works)

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main (main) where
 
-import SolveQuad (solve)
+import SolveQuad
+import Text.Read (readMaybe)
 
 main :: IO ()
-main = solve Equation {a = 1, b = 2, c = 3}
+main = do
+    input <- getLine
+    let eq = createEquation input
+    case eq of
+        Just a -> print $ solve a
+        Nothing -> error "Error while parsing"
+
+parseThreeNumbers :: String -> Maybe (Double, Double, Double)
+parseThreeNumbers input =
+    case map readMaybe (words input) of
+        [Just n1, Just n2, Just n3] -> Just (n1, n2, n3)
+        _ -> Nothing
+
+createEquation :: String -> Maybe Equation
+createEquation input = do
+    (f, s, t) <- parseThreeNumbers input
+    return Equation { a = f, b = s, c = t }
