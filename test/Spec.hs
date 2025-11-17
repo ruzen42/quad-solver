ed8c62a6c231c878691ca819de529397d84dd7e2
Yuriy Syrovetskiy <git-orbi@cblp.su>
2025-11-18 00:06:00 +0100

    Add a bug report

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,7 +4,7 @@
 
 module Main (main) where
 
-import Data.List (sort)
+import Data.Complex
 import Test.HUnit.Approx (assertApproxEqual)
 import Test.Tasty.HUnit
 import Test.Tasty.TH
@@ -18,15 +18,10 @@ infix 1 @?~
 
 case_twoRealRoots =
     case solve Equation{a = 1, b = -5, c = 6} of
-        Two r1 r2 -> do
-            actualRoot1 @?~ expectedRoot1
-            actualRoot2 @?~ expectedRoot2
-          where
-            [actualRoot1, actualRoot2] = sort [r1, r2]
+        Two actualRoot1 actualRoot2 -> do
+            actualRoot1 @?~ 2
+            actualRoot2 @?~ 3
         _ -> assertFailure "Expected two roots (Two), got something else"
-  where
-    expectedRoot1 = 2
-    expectedRoot2 = 3
 
 case_oneRealRoot = do
     case solve Equation{a = 1, b = -4, c = 4} of
@@ -35,9 +30,9 @@ case_oneRealRoot = do
 
 case_complexRoots =
     case solve Equation{a = 1, b = 0, c = 4} of
-        Complex real imag -> do
-            real @?~ 0
-            imag @?~ 2
+        Complex actualRoot1 actualRoot2 -> do
+            actualRoot1 @?~ 0 :+ (-2)
+            actualRoot2 @?~ 0 :+ 2
         _ ->
             assertFailure "Expected complex roots (Complex), got something else"
 
